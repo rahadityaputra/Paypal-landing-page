601bb7dafd68bf17bb2d5cad3b31ed964a459afc
rahadityaputra <rahadit.a.p123@gmail.com>
2024-10-07 17:24:22 +0700

    fix bug

--- script.js
+++ script.js
@@ -2,41 +2,30 @@ const menus = document.querySelectorAll('.list-menu li a[href="#"]');
 const megaMenuWrapper = document.querySelector(".mega-menu");
 const megaMenus = document.querySelectorAll(".mega-menu-item");
 
+let timeOutMegaMenu;
+
 const hideMegaMenu = (megaMenuElement) => {
-    setTimeout(() => {
-        megaMenuElement.classList.remove('active');
-    },  1000);
-
-}
-
-const showMegaMenu = (megaMenuElement) => { 
-    megaMenuElement.classList.add('active');
-}
-
-const addEventAtMenu = (menus, megaMenuElement, callBackShow, callBackHide  ) => {
-    
-    menus.forEach((menu) => {
-        menu.addEventListener('mouseover', function () {
-            callBackShow(megaMenuElement);
-        })
-
-        menu.addEventListener('mouseleave', function () {
-            callBackHide(megaMenuElement);
-        })
-    })
-
+  timeOutMegaMenu = setTimeout(() => {
+    megaMenuElement.classList.remove("active");
+  }, 1000);
 };
 
+const showMegaMenu = (megaMenuElement) => {
+  clearTimeout(timeOutMegaMenu);
+  megaMenuElement.classList.add("active");
+};
+
+const addEventAtMenu = (menus, megaMenuElement, callBackShow, callBackHide) => {
+  menus.forEach((menu) => {
+    menu.addEventListener("mouseover", function () {
+      callBackShow(megaMenuElement);
+    });
+
+    menu.addEventListener("mouseleave", function () {
+      callBackHide(megaMenuElement);
+    });
+  });
+};
 
 addEventAtMenu(menus, megaMenuWrapper, showMegaMenu, hideMegaMenu);
-// megaMenus.forEach((megaMenu) => {
-//     megaMenu.addEventListener("mouseenter", function () {
-//       megaMenuWrapper.classList.add("active");
-//     });
-//     megaMenu.addEventListener("mouseleave", function () {
-  
-//       setTimeout(() => {
-//         megaMenuWrapper.classList.remove("active");
-//       }, 1000);
-//     });
-//   });
+addEventAtMenu(megaMenus, megaMenuWrapper, showMegaMenu, hideMegaMenu);
