d9c70fe379bb76c3ae155794f282416c696c228b
rahadityaputra <rahadit.a.p123@gmail.com>
2024-10-08 11:52:59 +0700

    meraoikan kode supaya lebih modular

--- script.js
+++ script.js
@@ -1,31 +1,68 @@
-const menus = document.querySelectorAll('.list-menu li a[href="#"]');
+const menus = document.querySelectorAll(".list-menu li:has(a)");
 const megaMenuWrapper = document.querySelector(".mega-menu");
 const megaMenus = document.querySelectorAll(".mega-menu-item");
+// const icons = document.querySelectorAll('li .icon');
+
+const rotateIcon = (icon) => {
+  console.log(icon);
+
+  icon.classList.toggle("rotate-180");
+};
 
 let timeOutMegaMenu;
 
 const hideMegaMenu = (megaMenuElement) => {
   timeOutMegaMenu = setTimeout(() => {
-    megaMenuElement.classList.remove("active");
+    megaMenuElement.classList.add("hidden");
   }, 1000);
 };
 
 const showMegaMenu = (megaMenuElement) => {
   clearTimeout(timeOutMegaMenu);
-  megaMenuElement.classList.add("active");
+  megaMenuElement.classList.remove("hidden");
 };
 
-const addEventAtMenu = (menus, megaMenuElement, callBackShow, callBackHide) => {
-  menus.forEach((menu) => {
-    menu.addEventListener("mouseover", function () {
-      callBackShow(megaMenuElement);
-    });
+// function yang menerima argumen id target, dan show : true / false;
+const toggleMegaMenu = (target, show) => {
+  // menangkap element susuai id target
+  const megaMenuTarget = document.querySelector(
+    `.mega-menu-item[data-id="${target}"]`
+  );
+  // jika show = true , maka  show mega menu
+  if (show) {
+    // hide semua menu terlebih dahulu
+    megaMenus.forEach((megaMenu) => { 
+      megaMenu.classList.add("hidden");
+    })
 
-    menu.addEventListener("mouseleave", function () {
-      callBackHide(megaMenuElement);
-    });
+    // kemudian  show mega menu yang sesuai dengan id target
+    showMegaMenu(megaMenuTarget);
+
+  } else {
+    // jika  show = false maka hide mega menu
+    hideMegaMenu(megaMenuTarget);
+  }
+};
+
+// beri event untuk semua menu yang memiliki tag a di dalamnya
+menus.forEach((menu) => {
+  menu.addEventListener("mouseover", function () {
+    const target = menu.dataset.target;
+    if (target) {
+      toggleMegaMenu(target, true);
+    }
   });
-};
 
-addEventAtMenu(menus, megaMenuWrapper, showMegaMenu, hideMegaMenu);
-addEventAtMenu(megaMenus, megaMenuWrapper, showMegaMenu, hideMegaMenu);
+  menu.addEventListener("mouseleave", function () {
+    const target = menu.dataset.target;
+    if (target) {
+      toggleMegaMenu(target, false);
+    }
+  });
+});
+
+// beri event untuk semua mega menu
+megaMenus.forEach((megaMenu) => {
+  megaMenu.addEventListener("mouseover", () => { toggleMegaMenu(megaMenu.dataset.id, true) });
+  megaMenu.addEventListener("mouseleave", () => { toggleMegaMenu(megaMenu.dataset.id, false) });
+})
